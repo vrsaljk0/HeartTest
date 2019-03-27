9972c27b5862f4b6c2c0ee5fe4fc1161e0ad6b66
Maja Vrsaljko <majavrsaljko78@gmail.com>
2019-03-27 15:16:14 +0100

    samo ADC

--- main.c
+++ main.c
@@ -1,87 +1,54 @@
 #define F_CPU 7372800UL
+
 #include <avr/io.h>
-#include <avr/interrupt.h>
 #include <util/delay.h>
+#include <stdlib.h>
+
 #include "lcd.h"
 
-static int beats = 0;
-static uint8_t tSS = 99;
-static uint8_t tS = 5;
-static uint8_t flag = 0;
+static int beats=0;
 
-void changeTime() {
-	char time[6];
+void writeLCD(uint16_t adc) {
+    lcd_clrscr();
 
-	time[0] = '0' + (tS / 10);
-	time[1] = '0' + (tS % 10);
-	time[2] = 's';
-	time[3] = 'e';
-	time[4] = 'c';
-	time[5] = '\0';
-
-	lcd_clrscr();
-	lcd_gotoxy(4, 1);
-	lcd_puts(time);
-	print(beats);
-}
-
-ISR(TIMER0_COMP_vect) {
-	tSS--;
+    char adcStr[16];
+    itoa(adc, adcStr, 10);
+    lcd_puts(adcStr);
 	
-	if (tSS == 0) {
-		tSS = 99;
-		
-		tS--;
-		
-		if (tS == 0) {
-			int res = 4*beats;
-			lcd_clrscr();
-			print(res);
-			flag = 1;
-		}
-		if(flag == 0) changeTime();
-	}
-}
-
-void print(int num){
 	char cast[4];
-	cast[0] = '0' + (num / 100);
-	cast[1] = '0' + ((num % 100)/10);
-	cast[2] = '0' + ((num%100)%10);
+	cast[0] = '0' + (beats / 100);
+	cast[1] = '0' + ((beats % 100)/10);
+	cast[2] = '0' + ((beats%100)%10);
 	cast[3] = '\0';
-	lcd_gotoxy(4, 0);
+	lcd_gotoxy(4, 1);
 	lcd_puts(cast);
-}
-void debounce(){
-	GICR &= ~_BV(INT1);
-	sei();
-	_delay_ms(500);
-	GIFR = _BV(INTF1);
-	GICR |= _BV(INT1);
-	cli();
+	
+	
 }
 
-ISR(INT1_vect) {
-	beats++;
-	debounce();
-}
 
-int main(void) {
+int main(void)
+{
 	DDRD = _BV(4);
-	TCCR1A = _BV(WGM10) | _BV(COM1B1);
-	TCCR1B = _BV(WGM12) | _BV(CS11) | _BV(CS10);
-	OCR1B = 50;
-	
-	TCCR0 = _BV(WGM01) | _BV(CS02) | _BV(CS00);
-	OCR0 = 72;
-	TIMSK = _BV(OCIE0);
-	
-	MCUCR = _BV(ISC10) | _BV(ISC11);
-	GICR = _BV(INT1);
-	
-	lcd_init(LCD_DISP_ON);
-	//print();
-	sei();
 
-	while (1);
+	TCCR1A = _BV(COM1B1) | _BV(WGM10);
+	TCCR1B = _BV(WGM12) | _BV(CS11);
+	OCR1B = 64;
+
+	lcd_init(LCD_DISP_ON);
+
+	ADMUX = _BV(REFS0);
+	ADCSRA = _BV(ADEN) | _BV(ADPS2) | _BV(ADPS1);
+
+	while (1) {
+		ADCSRA |= _BV(ADSC);
+
+		while (!(ADCSRA & _BV(ADIF)));
+		if(ADC < 350) {
+			beats++;
+		}
+		writeLCD(ADC);
+
+		_delay_ms(300);
+	}
 }
